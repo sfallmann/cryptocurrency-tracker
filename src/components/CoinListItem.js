aeb02c933a6fa35c2ef2159e0e511a75423f2aa5
Sean Fallman <sean.fallmann@retailbusinessservices.com>
2019-07-25 13:35:19 -0400

    Changed CoinListItem from class based PureComponent to functional (using React.memo)

--- src/components/CoinListItem.js
+++ src/components/CoinListItem.js
@@ -1,17 +1,13 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { ListItem } from 'react-native-elements';
 import icons from '../../assets/images/icons/128';
 
-export default class CoinListItem extends PureComponent {
+const CoinListItem = ({ item }) => (
+  <ListItem
+    title={item.name}
+    subtitle={`Price: $${item.priceUsd}`}
+    leftAvatar={{ source: icons[item.symbol.toLowerCase()] }}
+  />
+);
 
-  render() {
-    const { item } = this.props;
-    return (
-      <ListItem
-        title={item.name}
-        subtitle={`Price: $${item.priceUsd}`}
-        leftAvatar={{ source: icons[item.symbol.toLowerCase()] }}
-      />
-    );
-  }
-}
+export default React.memo(CoinListItem);
