75c28dd136ecb5e67b43bad0b176d218887c69dd
sfallmann <sean.fallmann@gmail.com>
2019-07-25 14:01:14 -0400

    Created CoinListFilter component for filtering the list

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Provider } from 'react-redux';
 import CoinList from './src/components/CoinList';
+import CoinListFilter from './src/components/CoinListFilter';
 import store from './src/store';
 
 
@@ -10,6 +11,7 @@ export default function App() {
 
     <Provider store={store}>
       <View style={styles.container}>
+        <CoinListFilter />
         <CoinList />
       </View>
     </Provider>
