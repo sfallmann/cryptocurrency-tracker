8cec349f86fb645882dad7de756902f5177c9ea9
Sean Fallman <sean.fallmann@retailbusinessservices.com>
2019-07-25 10:04:17 -0400

    Added CoinList to App.js component to test out CoinList and CoinListItem components Added react-native-elements ui package Created CoinListItem component to render coin information Created CoinList to display list of coins Added hard coded data in list.js to test components

--- App.js
+++ App.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import icons from './assets/images/icons/128';
+import { StyleSheet, View } from 'react-native';
+import CoinList from './src/components/CoinList';
 
-const coin = 'aion';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>   
-      <Image style={{ width: 50, height: 50 }} source={icons[coin]} />
+      <CoinList />
     </View>
   );
 }
@@ -17,7 +15,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ src/components/CoinList.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { FlatList } from 'react-native';
+import CoinListItem from './CoinListItem';
+import coinlist from './list';
+
+const priceSortedList = () => coinlist.sort((a, b) => {
+    const aVal = Number.parseFloat(a.priceUsd);
+    const bVal = Number.parseFloat(b.priceUsd);
+    return bVal - aVal;
+  });
+
+export default class CoinList extends Component {
+
+  keyExtractor = (item) => item.id.toString();
+  renderItem = ({ item }) => (<CoinListItem item={item} />)
+  
+  render() {
+    return (
+      <FlatList
+          keyExtractor={this.keyExtractor}
+          data={priceSortedList()}
+          renderItem={this.renderItem}
+      />
+    );
+  }
+}
