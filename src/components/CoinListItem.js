28ff8251515c0ab0c3a1f853814d0a911fdfcf36
sfallmann <jorundr@hotmail.com>
2019-07-28 15:25:26 -0400

    Moved all functions inside of CoinListItem component Removed function for icon in CoinListItem and added the expressiion inline

--- src/components/CoinListItem.js
+++ src/components/CoinListItem.js
@@ -3,21 +3,18 @@ import { TouchableOpacity } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import icons from '../../assets/images/icons/128';
 
-const formatPrice = (priceStr) => {
-  let price = Number.parseFloat(priceStr);
-
-  if (Number.isNaN(price)) return 'N/A';
-
-  price = (Math.round(price * 1000)) / 1000;
-  return price.toFixed(3);
-}
 
 const CoinListItem = ({ item }) => {
 
   const [isFavorite, toggleFavorite] = useState(false);
+
+  const formatPrice = (priceStr) => {
+    let price = Number.parseFloat(priceStr);
   
-  const getIconName = (isFavorite) => {
-    return isFavorite ? 'heart' : 'hearto';
+    if (Number.isNaN(price)) return 'N/A';
+  
+    price = (Math.round(price * 1000)) / 1000;
+    return price.toFixed(3);
   }
 
   const changeFavoriteStatus = () => {
@@ -32,7 +29,7 @@ const CoinListItem = ({ item }) => {
         leftAvatar={{ source: icons[item.symbol.toLowerCase()] }}
         containerStyle={styles.container}
         rightIcon={{ 
-          name: getIconName(isFavorite), 
+          name: isFavorite ? 'heart' : 'hearto', 
           type: 'antdesign', 
           onPress: changeFavoriteStatus, 
           size: 36,
